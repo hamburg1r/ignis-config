3d784fc42c0f0ffac4243bcd86b849286b5fa825
Hamburgir <shantnuprofid@gmail.com>
2025-12-07 23:13:55 +0530

    Added initial implementation of window for searching stuff

--- desktop/config.py
+++ desktop/config.py
@@ -3,24 +3,29 @@ from ignis import utils
 from ignis.css_manager import CssInfoPath, CssManager
 from desktop.windows.bar import Bar
 from desktop.windows.osd import Osd
+from desktop.windows.search import SearchList
 
 css_manager=CssManager.get_default()
 
 def patch_style_scss(path):
-	with open(path) as file:
-		string = file.read()
+    with open(path) as file:
+        string = file.read()
 
-	return utils.sass_compile(
-        string=string, extra_args=["--load-path", utils.get_current_dir()]
-    )
+        return utils.sass_compile(
+            string=string, extra_args=["--load-path", utils.get_current_dir()]
+        )
 
 css_manager.apply_css(
     CssInfoPath(
         name="main",
         path=os.path.join(utils.get_current_dir(), "style.scss"),
+        priority="user",
         compiler_function=patch_style_scss,
     )
 )
 
+print(css_manager.list_css_infos())
+
 Bar(0)
 Osd()
+SearchList()
--- flake.nix
+++ flake.nix
@@ -39,15 +39,25 @@
           # default = hello;
         };
         devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gtk3
+            gtk4
+            glib
+          ];
           nativeBuildInputs = with pkgs; [
             (python3.withPackages(ps: with ps; [
               ipython
               ignisDeps
               gobject-introspection
               pygobject3
+              pygobject-stubs
             ]))
             basedpyright
+            gobject-introspection
           ];
+          shellHook = ''
+            export GI_TYPELIB_PATH="${pkgs.gtk3}/lib/girepository-1.0:${pkgs.gtk4}/lib/girepository-1.0:${pkgs.glib}/lib/girepository-1.0:$GI_TYPELIB_PATH"
+          '';
         };
       }
     );
