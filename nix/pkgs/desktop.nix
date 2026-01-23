1397681b354cdda98a5ed40a83645ad140a36fb1
Hamburgir <shantnuprofid@gmail.com>
2026-01-23 15:50:40 +0530

    fix: migration from setuptools to pyproject

--- nix/pkgs/desktop.nix
+++ nix/pkgs/desktop.nix
@@ -3,7 +3,7 @@ pkgs.python3Packages.buildPythonApplication {
   pname = "ignis-desktop";
   version = "0.1.0";
   src = src;
-  format = "setuptools";
+  format = "pyproject";
   doCheck = false;
   
   nativeBuildInputs = [
